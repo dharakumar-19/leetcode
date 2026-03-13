a524808127ee5f72f8b99afa34ccb53b60627b4e
dharakumar-19 <dharakumar0224@gmail.com>
2026-03-13 13:57:11 +0530

    Time: 2 ms (43.25%) | Memory: 43.2 MB (83.47%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int s : students){
+            q.add(s);
+        }
+        
+        int index = 0;   
+        int count = 0;   
+        
+        while(!q.isEmpty() && count < q.size()){
+            
+            if(q.peek() == sandwiches[index]){
+                q.poll();     
+                index++;
+                count = 0; 
+            }
+            else{
+                q.add(q.poll());
+                count++;
+            }
+        }
+        
+        return q.size();
+    }
+}
